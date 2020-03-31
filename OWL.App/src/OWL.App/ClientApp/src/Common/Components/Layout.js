04fb9679558c2690ccf6db2bc5088a0e4fbd245f
Scott-Resdiary <scott.copeland@resdiary.com>
2020-03-31 16:25:31 +0100

    Set up some default Icons and Hooks

--- OWL.App/src/OWL.App/ClientApp/src/Common/Components/Layout.js
+++ OWL.App/src/OWL.App/ClientApp/src/Common/Components/Layout.js
@@ -1,16 +1,17 @@
 import React, { Component } from 'react';
 import { Container } from 'reactstrap';
+import SidePanel from './SidePanel/SidePanel'
 
 export class Layout extends Component {
-  static displayName = Layout.name;
+    static displayName = Layout.name;
 
-  render () {
-    return (
-      <div>
-        <Container>
-          {this.props.children}
-        </Container>
-      </div>
-    );
-  }
+    render () {
+        return (
+            <div>
+                <Container>
+                    {this.props.children}
+                </Container>
+            </div>
+        );
+    }
 }
